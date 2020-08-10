1051c7eee07d38f6b096daaac468ec4295bfd58e
wensimin <hukaizhiyu2@gmail.com>
2020-08-10 22:50:03 +0800

    Initial commit

---
+++ src/main/java/com/github/wensimin/ashioarae/AshioaraeApplication.java
@@ -0,0 +1,13 @@
+package com.github.wensimin.ashioarae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AshioaraeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AshioaraeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/wensimin/ashioarae/AshioaraeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.wensimin.ashioarae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AshioaraeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
