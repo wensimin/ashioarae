0035bb8e40443cead42e397866db57f6e33043ac
wensimin <hukaizhiyu2@gmail.com>
2020-08-20 06:31:19 +0800

    cors处理

---
+++ src/main/java/com/github/wensimin/ashioarae/config/WebAppConfigurer.java
@@ -0,0 +1,19 @@
+package com.github.wensimin.ashioarae.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * web app 配置
+ */
+@Configuration
+public class WebAppConfigurer implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*");
+    }
+}
--- src/main/java/com/github/wensimin/ashioarae/config/WebSecurityConfig.java
+++ src/main/java/com/github/wensimin/ashioarae/config/WebSecurityConfig.java
@@ -23,6 +23,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception
     {
         http
+                .cors().and()
                 .csrf().disable()
                 .authorizeRequests().anyRequest().authenticated()
                 .and()
