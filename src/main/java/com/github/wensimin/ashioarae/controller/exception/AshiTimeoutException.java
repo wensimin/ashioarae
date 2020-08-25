b5b1291ff819aec571e4b5272b137cc22eca31da
wensimin <hukaizhiyu2@gmail.com>
2020-08-25 21:48:13 +0800

    code clear

--- src/main/java/com/github/wensimin/ashioarae/controller/exception/AshiTimeoutException.java
+++ src/main/java/com/github/wensimin/ashioarae/controller/exception/AshiTimeoutException.java
@@ -1,5 +1,8 @@
 package com.github.wensimin.ashioarae.controller.exception;
 
+/**
+ * 超时异常
+ */
 public class AshiTimeoutException extends AshiException {
 
     public AshiTimeoutException(String message) {
