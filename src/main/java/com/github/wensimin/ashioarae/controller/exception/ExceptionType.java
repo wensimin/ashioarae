b3eb953bd6f40d9760dee83fe34def332764376a
wensimin <hukaizhiyu2@gmail.com>
2020-09-19 12:57:15 +0800

    异常类型注释

--- src/main/java/com/github/wensimin/ashioarae/controller/exception/ExceptionType.java
+++ src/main/java/com/github/wensimin/ashioarae/controller/exception/ExceptionType.java
@@ -4,7 +4,17 @@ package com.github.wensimin.ashioarae.controller.exception;
  * 异常类型枚举
  */
 public enum ExceptionType {
+    /**
+     * 给开发者展示的error message
+     */
     error,
+    /**
+     * cookie的error
+     */
     cookie,
+    /**
+     * 超时性质error
+     * 客户端可进行重试
+     */
     timeout
 }
